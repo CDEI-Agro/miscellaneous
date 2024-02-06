622a5a50f03f737491c6bb5e0b18de630b1f76a9
ignasimUPC <125452989+ignasimUPC@users.noreply.github.com>
2024-02-06 15:14:17 +0100

    Update getting_started.sh

--- getting_started.sh
+++ getting_started.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# Install ROS 2 Humble
+#### Update and upgrade:
 sudo apt update
-sudo apt install -y curl
+sudo apt upgrade
 
+#### Useful programs:
+# Install curl
+sudo apt install -y curl
+# Install VSCode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
+# Install ifconfig
+sudo apt install net-tools -y
+# Install terminator
+sudo apt install terminator -y
+
+#### ROS Humble installation:
 # First ensure that the Ubuntu Universe repository is enabled.
 sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
 
 # Now add the ROS 2 GPG key with apt.
-sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Then add the repository to your sources list.
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# Update your apt repository caches after setting up the repositories.
+# Update again your apt repository caches after setting up the repositories.
 sudo apt update
 sudo apt upgrade
 
