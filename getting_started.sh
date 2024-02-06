4e5cc3c18aecea8f646c48a274f099bf491600e4
ignasimUPC <125452989+ignasimUPC@users.noreply.github.com>
2024-02-06 12:49:08 +0100

    Update getting_started.sh

--- getting_started.sh
+++ getting_started.sh
@@ -3,26 +3,40 @@
 # Install ROS 2 Humble
 sudo apt update
 sudo apt install -y curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu focal main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+# First ensure that the Ubuntu Universe repository is enabled.
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+# Now add the ROS 2 GPG key with apt.
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# Then add the repository to your sources list.
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Update your apt repository caches after setting up the repositories.
 sudo apt update
-sudo apt install -y ros-humble-desktop
+sudo apt upgrade
+
+# Desktop Install (Recommended): ROS, RViz, demos, tutorials.
+sudo apt install ros-humble-desktop
 
 # Source ROS 2 setup.bash
 source /opt/ros/humble/setup.bash
 
 # Create ROS 2 workspace
-mkdir -p ~/ros2ws/src
-cd ~/ros2ws
+mkdir -p ~/ws/src
+cd ~/ws
 
-# Clone the package
-git clone https://github.com/CDEI-Agro/agri_bot src/agri_bot
+# # Clone the package
+# git clone https://github.com/CDEI-Agro/agri_bot src/agri_bot
 
-# Install dependencies
-rosdep install --from-paths src --ignore-src -r -y
+# # Install dependencies
+# rosdep install --from-paths src --ignore-src -r -y
 
-# Build the package
-colcon build
+# # Build the package
+# colcon build
 
-# Source the workspace
-source install/setup.bash
+# # Source the workspace
+# source install/setup.bash
